8cde329b619b235657f904fa8d9f1aeada038c77
Hao Zhang <albert.zhang.hit@gmail.com>
2021-03-23 03:17:37 +0800

    update

--- circle_fwi.py
+++ circle_fwi.py
@@ -9,108 +9,160 @@ import matplotlib.pyplot as plt
 from fwi import Filter, fm_multi, fwi_obj_multi, fwi_loss
 from misfit import least_square, qWasserstein
 from bfm import bfm
-# Set up velocity model
-shape = (101, 101)      # Number of grid points (nx, nz).
-spacing = (10., 10.)    # Grid spacing in m. The domain size is now 1km by 1km.
-origin = (0, 0)         # Need origin to define relative source and receiver locations.
-nbl = 40
-dt = 1.
-precond = True
-# True model
-true_model = demo_model('circle-isotropic', vp_circle=3.6, vp_background=3,
-    origin=origin, shape=shape, spacing=spacing, nbl=nbl, dt=dt)
 
-# Initial model
-init_model = demo_model('circle-isotropic', vp_circle=3, vp_background=3,
-    origin=origin, shape=shape, spacing=spacing, nbl=nbl, dt=dt)
+import argparse, os, time, shutil
 
-bathy_mask = np.ones(shape, dtype=np.float32)
-#init_model._dt = true_model.critical_dt
-# Set up acquisiton geometry
-t0 = 0.
-tn = 1000. 
-f0 = 0.010
-resample_dt = 5
-# Set up source geometry, but define 5 sources instead of just one.
-nsources = 11
-src_coordinates = np.empty((nsources, 2))
-src_coordinates[:, 1] = np.linspace(0, true_model.domain_size[0], num=nsources)
-src_coordinates[:, 0] = 20.  # Source depth is 20m
+parser = argparse.ArgumentParser(description='Full waveform inversion')
+parser.add_argument('--misfit', type=int, default=0, choices=[0, 1, 2], 
+			help='misfit function type:0=least square/1=1d W2/2=2d W2')
+parser.add_argument('--precond', type=int, default=1, help='apply precondition')
+parser.add_argument('--odir', type=str, default='./result', 
+			help='directory to output result')
+parser.add_argument('--bathy', type=int, default=1, help='apply bathy mask')
+parser.add_argument('--check-gradient', type=int, default=1, 
+			help='check the gradient at 1st iteration')
 
-# Initialize receivers for synthetic and imaging data
-nreceivers = shape[0]
-rec_coordinates = np.empty((nreceivers, 2))
-rec_coordinates[:, 1] = np.linspace(spacing[0], true_model.domain_size[0] - spacing[0], num=nreceivers)
-rec_coordinates[:, 0] = 980.    # Receiver depth
-# Set up geometry objects for observed and predicted data
-geometry1 = AcquisitionGeometry(true_model, rec_coordinates, src_coordinates, t0, tn, f0=f0, src_type='Ricker')
-geometry0 = AcquisitionGeometry(init_model, rec_coordinates, src_coordinates, t0, tn, f0=f0, src_type='Ricker')
-geometry1.resample(resample_dt)
-geometry0.resample(resample_dt)
-# client = Client(processes=False)
-obs = fm_multi(geometry1, save=False)
+if __name__=='__main__':
+	# Parse argument
+	args = parser.parse_args()
 
-filt_func = Filter(filter_type='highpass', freqmin=2, 
-				corners=10, df=1000/resample_dt, axis=-2)
+	result_dir = args.odir
+	if not os.path.exists(result_dir):
+		os.makedirs(result_dir)
+	misfit_type = args.misfit
+	precond = args.precond
+	use_bathy = args.bathy
+	check_gradient = args.check_gradient
 
-# syn = fm_multi(geometry0, save=False)
-# obs_data = obs[int(nsources/2)].data
-# syn_data = syn[int(nsources/2)].data
-# plot_shotrecord(obs_data, true_model, t0, tn)
-# plot_shotrecord(filt_func(obs_data), true_model, t0, tn)
-# plot_shotrecord(syn_data-obs_data, true_model, t0, tn)
-# plot_shotrecord(filt_func(syn_data)-filt_func(obs_data), true_model, t0, tn)
+	# Set up velocity model
+	shape = (101, 101)      # Number of grid points (nx, nz).
+	spacing = (10., 10.)    # Grid spacing in m. The domain size is now 1km by 1km.
+	origin = (0, 0)         # Need origin to define relative source and receiver locations.
+	nbl = 40
+	dt = 1.
 
-qWmetric1d = qWasserstein(gamma=1.01, method='1d')
-bfm_solver = bfm(num_steps=10, step_scale=1.)
-qWmetric2d = qWasserstein(gamma=1.01, method='2d', bfm_solver=bfm_solver)
+	# True model
+	true_model = demo_model('circle-isotropic', vp_circle=3.6, vp_background=3,
+	    origin=origin, shape=shape, spacing=spacing, nbl=nbl, dt=dt)
 
-misfit_func = least_square
-#misfit_func = qWmetric1d
-#misfit_func = qWmetric2d
+	# Initial model
+	init_model = demo_model('circle-isotropic', vp_circle=3, vp_background=3,
+	    origin=origin, shape=shape, spacing=spacing, nbl=nbl, dt=dt)
 
-# # Gradient check
-f, g = fwi_obj_multi(geometry0, obs, misfit_func, 
-				filt_func)
-plot_image(g.reshape(shape), cmap='cividis')
+	bathy_mask = np.ones(shape, dtype=np.float32)
+	if not use_bathy:
+		bathy_mask = None	
 
-model_err = []
-def fwi_callback(xk):
-	nbl = true_model.nbl
-	v = true_model.vp.data[nbl:-nbl, nbl:-nbl]
-	m = 1. / (v.reshape(-1).astype(np.float64))**2
-	model_err.append(np.linalg.norm((xk-m)/m))
+	# Set up acquisiton geometry
+	t0 = 0.
+	tn = 1000. 
+	f0 = 0.010
+	resample_dt = 5
+	# Set up source geometry, but define 5 sources instead of just one.
+	nsources = 11
+	src_coordinates = np.empty((nsources, 2))
+	src_coordinates[:, 1] = np.linspace(0, true_model.domain_size[0], num=nsources)
+	src_coordinates[:, 0] = 20.  # Source depth is 20m
 
-# Box contraints
-vmin = 2.5    # do not allow velocities slower than water
-vmax = 4.0
-bounds = [(1.0/vmax**2, 1.0/vmin**2) for _ in range(np.prod(init_model.shape))]    # in [s^2/km^2]
+	# Initialize receivers for synthetic and imaging data
+	nreceivers = shape[0]
+	rec_coordinates = np.empty((nreceivers, 2))
+	rec_coordinates[:, 1] = np.linspace(spacing[0], true_model.domain_size[0] - spacing[0], num=nreceivers)
+	rec_coordinates[:, 0] = 980.    # Receiver depth
+	# Set up geometry objects for observed and predicted data
+	geometry1 = AcquisitionGeometry(true_model, rec_coordinates, src_coordinates, t0, tn, f0=f0, src_type='Ricker')
+	geometry0 = AcquisitionGeometry(init_model, rec_coordinates, src_coordinates, t0, tn, f0=f0, src_type='Ricker')
+	geometry1.resample(resample_dt)
+	geometry0.resample(resample_dt)
+	# client = Client(processes=False)
+	obs = fm_multi(geometry1, save=False)
 
-# Initial guess
-v0 = init_model.vp.data[init_model.nbl:-init_model.nbl, init_model.nbl:-init_model.nbl]
-m0 = 1.0 / (v0.reshape(-1).astype(np.float64))**2
+	filt_func = Filter(filter_type='highpass', freqmin=2, 
+					corners=10, df=1000/resample_dt, axis=-2)
 
-# FWI with L-BFGS
-ftol = 2e-9 # converge when ftol <= _factor * EPSMCH
-maxiter = 50
-maxls = 5
-gtol = 1e-6
-stepsize = 1e-8 # minimize default step size
-"""
-scipy.optimize.minimize(fun, x0, args=(), method='L-BFGS-B', jac=None, 
-	bounds=None, tol=None, callback=None, 
-	options={'disp': None, 'maxcor': 10, 'ftol': 2.220446049250313e-09, 
-	'gtol': 1e-05, 'eps': 1e-08, 'maxfun': 15000, 'maxiter': 15000, 
-	'iprint': - 1, 'maxls': 20, 'finite_diff_rel_step': None})
-"""
-result = optimize.minimize(fwi_loss, m0, 
-			args=(geometry0, obs, misfit_func, filt_func, bathy_mask, precond), 
-			method='L-BFGS-B', jac=True, 
-    		callback=fwi_callback, bounds=bounds, 
-    		options={'ftol':ftol, 'maxiter':maxiter, 'disp':True,
-    				'eps':stepsize,
-    				'maxls':maxls, 'gtol':gtol, 'iprint':1,
-    		})
-# Plot FWI result
-vp = 1.0/np.sqrt(result['x'].reshape(true_model.shape))
-plot_image(vp, vmin=vmin, vmax=vmax, cmap="cividis")
+	# syn = fm_multi(geometry0, save=False)
+	# obs_data = obs[int(nsources/2)].data
+	# syn_data = syn[int(nsources/2)].data
+	# plot_shotrecord(obs_data, true_model, t0, tn)
+	# plot_shotrecord(filt_func(obs_data), true_model, t0, tn)
+	# plot_shotrecord(syn_data-obs_data, true_model, t0, tn)
+	# plot_shotrecord(filt_func(syn_data)-filt_func(obs_data), true_model, t0, tn)
+
+	qWmetric1d = qWasserstein(gamma=1.01, method='1d')
+	bfm_solver = bfm(num_steps=10, step_scale=1.)
+	qWmetric2d = qWasserstein(gamma=1.01, method='2d', bfm_solver=bfm_solver)
+
+	if misfit_type == 0:
+		misfit_func = least_square
+	if misfit_type == 1:
+		misfit_func = qWmetric1d
+	if misfit_type == 2:
+		misfit_func = qWmetric2d
+
+	# Gradient check
+	if check_gradient:
+		f, g = fwi_obj_multi(geometry0, obs, misfit_func, 
+						filt_func, precond=precond)
+		plot_image(g.reshape(shape), cmap='cividis')
+
+	model_err = []
+	def fwi_callback(xk):
+		nbl = true_model.nbl
+		v = true_model.vp.data[nbl:-nbl, nbl:-nbl]
+		m = 1. / (v.reshape(-1).astype(np.float64))**2
+		model_err.append(np.linalg.norm((xk-m)/m))
+
+	# Box contraints
+	vmin = 2.5    # do not allow velocities slower than water
+	vmax = 4.0
+	bounds = [(1.0/vmax**2, 1.0/vmin**2) for _ in range(np.prod(init_model.shape))]    # in [s^2/km^2]
+
+	# Initial guess
+	v0 = init_model.vp.data[init_model.nbl:-init_model.nbl, init_model.nbl:-init_model.nbl]
+	m0 = 1.0 / (v0.reshape(-1).astype(np.float64))**2
+
+	# FWI with L-BFGS
+	ftol = 2e-9 # converge when ftol <= _factor * EPSMCH
+	maxiter = 50
+	maxls = 5
+	gtol = 1e-6
+	stepsize = 1e-8 # minimize default step size
+	"""
+	scipy.optimize.minimize(fun, x0, args=(), method='L-BFGS-B', jac=None, 
+		bounds=None, tol=None, callback=None, 
+		options={'disp': None, 'maxcor': 10, 'ftol': 2.220446049250313e-09, 
+		'gtol': 1e-05, 'eps': 1e-08, 'maxfun': 15000, 'maxiter': 15000, 
+		'iprint': - 1, 'maxls': 20, 'finite_diff_rel_step': None})
+	"""
+	result = optimize.minimize(fwi_loss, m0, 
+				args=(geometry0, obs, misfit_func, filt_func, bathy_mask, precond), 
+				method='L-BFGS-B', jac=True, 
+	    		callback=fwi_callback, bounds=bounds, 
+	    		options={'ftol':ftol, 'maxiter':maxiter, 'disp':True,
+	    				'eps':stepsize,
+	    				'maxls':maxls, 'gtol':gtol, 'iprint':1,
+	    		})
+	# Plot FWI result
+	vp = 1.0/np.sqrt(result['x'].reshape(true_model.shape))
+
+	vp.tofile(os.path.join(result_dir, "circle_result_misfit_"+str(misfit_type)))
+
+	with open(os.path.join(result_dir, "circle_model_err_info_"+str(misfit_type)+'.txt')) as file:
+		for item in model_err:
+			f.write("%s\n" % str(item))
+	try:
+		useful_info = []
+		with open('nohup.out', 'rt') as file:
+			for line in file:
+				if line.find('Operator') < 0:
+					useful_info.append(line)
+		with open(os.path.join(result_dir, 'circle_optim_info_'+str(misfit_type)+'.txt')) as file:
+			for item in useful_info:
+				f.write("%s\n" % item)
+		nohup_file = 'circle_nohup_'+str(misfit_type)+'.out'
+		os.rename('nohup.out', nohup_file)
+		shutil.move(nohup_file, os.path.join(result_dir, nohup_file))
+	except:
+		pass
+	plot_image(vp, vmin=vmin, vmax=vmax, cmap="jet")
+
--- marmousi_fwi.py
+++ marmousi_fwi.py
@@ -10,113 +10,161 @@ from fwi import Filter, fm_multi, fwi_obj_multi, fwi_loss
 from misfit import least_square, qWasserstein
 from bfm import bfm
 
-# Setup velocity model
-shape = (300, 106)      # Number of grid points (nx, nz).
-spacing = (30., 30.)    # Grid spacing in m. The domain size is now 1km by 1km.
-origin = (0, 0)         # Need origin to define relative source and receiver locations.
-space_order = 6
-nbl = 40
-free_surface = False
-dt = 2.
-precond = True
+import argparse, os, time, shutil
 
-true_vp = np.fromfile("./model_data/SMARMN/vp.true", dtype=np.float32).reshape(shape)/1000
-smooth_vp = np.fromfile("./model_data/SMARMN/vp.smooth1", dtype=np.float32).reshape(shape)/1000
-bathy_mask = np.ones(shape, dtype=np.float32)
-bathy_mask[:, :6] = 0
+parser = argparse.ArgumentParser(description='Full waveform inversion')
+parser.add_argument('--misfit', type=int, default=0, choices=[0, 1, 2], 
+			help='misfit function type:0=least square/1=1d W2/2=2d W2')
+parser.add_argument('--precond', type=int, default=1, help='apply precondition')
+parser.add_argument('--odir', type=str, default='./result', 
+			help='directory to output result')
+parser.add_argument('--bathy', type=int, default=1, help='apply bathy mask')
+parser.add_argument('--check-gradient', type=int, default=1, 
+			help='check the gradient at 1st iteration')
 
-true_model = Model(origin=origin, spacing=spacing, 
-				shape=shape, space_order=space_order, vp=true_vp, 
-				nbl=nbl, fs=free_surface, dt=dt)
-init_model = Model(origin=origin, spacing=spacing, 
-				shape=shape, space_order=space_order, vp=smooth_vp, 
-				nbl=nbl, fs=free_surface, dt=dt)
+if __name__=='__main__':
+	# Parse argument
+	args = parser.parse_args()
 
-# Set up acquisiton geometry
-t0 = 0.
-tn = 4500. 
-f0 = 0.005
-resample_dt = 5
-# Set up source geometry, but define 5 sources instead of just one.
-nsources = 31
-src_coordinates = np.empty((nsources, 2))
-src_coordinates[:, 0] = np.linspace(0, true_model.domain_size[0], num=nsources)
-src_coordinates[:, -1] = 30.  # Source depth is 20m
+	result_dir = args.odir
+	if not os.path.exists(result_dir):
+		os.makedirs(result_dir)
+	misfit_type = args.misfit
+	precond = args.precond
+	use_bathy = args.bathy
+	check_gradient = args.check_gradient
 
-# Initialize receivers for synthetic and imaging data
-nreceivers = shape[0]
-rec_coordinates = np.empty((nreceivers, 2))
-rec_coordinates[:, 0] = np.linspace(spacing[0], true_model.domain_size[0] - spacing[0], num=nreceivers)
-rec_coordinates[:, 1] = 30.    # Receiver depth
+	# Setup velocity model
+	shape = (300, 106)      # Number of grid points (nx, nz).
+	spacing = (30., 30.)    # Grid spacing in m. The domain size is now 1km by 1km.
+	origin = (0, 0)         # Need origin to define relative source and receiver locations.
+	space_order = 6
+	nbl = 40
+	free_surface = False
+	dt = 3.
 
-# Set up geometry objects for observed and predicted data
+	true_vp = np.fromfile("./model_data/SMARMN/vp.true", dtype=np.float32).reshape(shape)/1000
+	smooth_vp = np.fromfile("./model_data/SMARMN/vp.smooth0", dtype=np.float32).reshape(shape)/1000
+	bathy_mask = np.ones(shape, dtype=np.float32)
+	bathy_mask[:, :7] = 0
+	if not use_bathy:
+		bathy_mask = None
 
-geometry1 = AcquisitionGeometry(true_model, rec_coordinates, src_coordinates, t0, tn, 
-				f0=f0, src_type='Ricker')
-geometry0 = AcquisitionGeometry(init_model, rec_coordinates, src_coordinates, t0, tn, 
-				f0=f0, src_type='Ricker')
-geometry1.resample(resample_dt)
-geometry0.resample(resample_dt)
-#plot_velocity(true_model, source=geometry1.src_positions, receiver=geometry1.rec_positions[::4, :])
+	true_model = Model(origin=origin, spacing=spacing, 
+					shape=shape, space_order=space_order, vp=true_vp, 
+					nbl=nbl, fs=free_surface, dt=dt)
+	init_model = Model(origin=origin, spacing=spacing, 
+					shape=shape, space_order=space_order, vp=smooth_vp, 
+					nbl=nbl, fs=free_surface, dt=dt)
 
-obs = fm_multi(geometry1, save=False)
-#print(obs[2].data.shape)
-#plot_shotrecord(obs[2].data, true_model, t0, tn)
+	# Set up acquisiton geometry
+	t0 = 0.
+	tn = 4500. 
+	f0 = 0.005
+	resample_dt = 10
+	# Set up source geometry, but define 5 sources instead of just one.
+	nsources = 31
+	src_coordinates = np.empty((nsources, 2))
+	src_coordinates[:, 0] = np.linspace(0, true_model.domain_size[0], num=nsources)
+	src_coordinates[:, -1] = 30.  # Source depth is 20m
 
-filt_func = Filter(filter_type='highpass', freqmin=2, 
-			corners=10, df=1000/resample_dt, axis=-2)
+	# Initialize receivers for synthetic and imaging data
+	nreceivers = shape[0]
+	rec_coordinates = np.empty((nreceivers, 2))
+	rec_coordinates[:, 0] = np.linspace(spacing[0], true_model.domain_size[0] - spacing[0], num=nreceivers)
+	rec_coordinates[:, 1] = 30.    # Receiver depth
 
-#filted_obs = filt_func(obs[1].data)
-#plot_shotrecord(filted_obs, true_model, t0, tn)
+	# Set up geometry objects for observed and predicted data
 
-qWmetric1d = qWasserstein(gamma=1.01, method='1d')
-bfm_solver = bfm(num_steps=10, step_scale=1.)
-qWmetric2d = qWasserstein(gamma=1.01, method='2d', bfm_solver=bfm_solver)
+	geometry1 = AcquisitionGeometry(true_model, rec_coordinates, src_coordinates, t0, tn, 
+					f0=f0, src_type='Ricker')
+	geometry0 = AcquisitionGeometry(init_model, rec_coordinates, src_coordinates, t0, tn, 
+					f0=f0, src_type='Ricker')
+	geometry1.resample(resample_dt)
+	geometry0.resample(resample_dt)
+	#plot_velocity(true_model, source=geometry1.src_positions, receiver=geometry1.rec_positions[::4, :])
 
-misfit_func = least_square
-#misfit_func = qWmetric1d
-#misfit_func = qWmetric2d
+	obs = fm_multi(geometry1, save=False)
+	#print(obs[2].data.shape)
+	#plot_shotrecord(obs[2].data, true_model, t0, tn)
 
-# Gradient check
-f, g = fwi_obj_multi(geometry0, obs, misfit_func, 
-				filt_func)
-plot_image(g.reshape(shape), cmap='cividis')
+	filt_func = Filter(filter_type='highpass', freqmin=2, 
+				corners=6, df=1000/resample_dt, axis=-2)
 
-model_err = []
-def fwi_callback(xk):
-	m = 1. / (true_vp.reshape(-1).astype(np.float64))**2
-	model_err.append(np.linalg.norm((xk-m)/m))
+	#filted_obs = filt_func(obs[1].data)
+	#plot_shotrecord(filted_obs, true_model, t0, tn)
 
-# Box contraints
-vmin = 1.4    # do not allow velocities slower than water
-vmax = 5.0
-bounds = [(1.0/vmax**2, 1.0/vmin**2) for _ in range(np.prod(shape))]    # in [s^2/km^2]
+	qWmetric1d = qWasserstein(gamma=1.01, method='1d')
+	bfm_solver = bfm(num_steps=10, step_scale=1.)
+	qWmetric2d = qWasserstein(gamma=1.01, method='2d', bfm_solver=bfm_solver)
 
-m0 = 1./(smooth_vp.reshape(-1).astype(np.float64))**2
+	if misfit_type == 0:
+		misfit_func = least_square
+	if misfit_type == 1:
+		misfit_func = qWmetric1d
+	if misfit_type == 2:
+		misfit_func = qWmetric2d
 
-# FWI with L-BFGS
-ftol = 2e-9 # converge when ftol <= _factor * EPSMCH
-maxiter = 10
-maxls = 5
-gtol = 1e-6
-stepsize = 1e-8 # minimize default step size
-"""
-scipy.optimize.minimize(fun, x0, args=(), method='L-BFGS-B', jac=None, 
-	bounds=None, tol=None, callback=None, 
-	options={'disp': None, 'maxcor': 10, 'ftol': 2.220446049250313e-09, 
-	'gtol': 1e-05, 'eps': 1e-08, 'maxfun': 15000, 'maxiter': 15000, 
-	'iprint': - 1, 'maxls': 20, 'finite_diff_rel_step': None})
-"""
+	# Gradient check
+	if check_gradient:
+		f, g = fwi_obj_multi(geometry0, obs, misfit_func, 
+						filt_func, precond=precond)
+		plot_image(g.reshape(shape), cmap='cividis')
 
-result = optimize.minimize(fwi_loss, m0, 
-			args=(geometry0, obs, misfit_func, filt_func, bathy_mask, precond), 
-			method='L-BFGS-B', jac=True, 
-    		callback=fwi_callback, bounds=bounds, 
-    		options={'ftol':ftol, 'maxiter':maxiter, 'disp':True,
-    				'eps':stepsize,
-    				'maxls':maxls, 'gtol':gtol, 'iprint':1,
-    		})
+	model_err = []
+	def fwi_callback(xk):
+		m = 1. / (true_vp.reshape(-1).astype(np.float64))**2
+		model_err.append(np.linalg.norm((xk-m)/m))
 
-# Plot FWI result
-vp = 1.0/np.sqrt(result['x'].reshape(shape))
-plot_image(vp, vmin=vmin, vmax=vmax, cmap="cividis")
+	# Box contraints
+	vmin = 1.4    # do not allow velocities slower than water
+	vmax = 5.0
+	bounds = [(1.0/vmax**2, 1.0/vmin**2) for _ in range(np.prod(shape))]    # in [s^2/km^2]
+
+	m0 = 1./(smooth_vp.reshape(-1).astype(np.float64))**2
+
+	# FWI with L-BFGS
+	ftol = 2e-9 # converge when ftol <= _factor * EPSMCH
+	maxiter = 300
+	maxls = 5
+	gtol = 1e-6
+	stepsize = 1e-8 # minimize default step size
+	"""
+	scipy.optimize.minimize(fun, x0, args=(), method='L-BFGS-B', jac=None, 
+		bounds=None, tol=None, callback=None, 
+		options={'disp': None, 'maxcor': 10, 'ftol': 2.220446049250313e-09, 
+		'gtol': 1e-05, 'eps': 1e-08, 'maxfun': 15000, 'maxiter': 15000, 
+		'iprint': - 1, 'maxls': 20, 'finite_diff_rel_step': None})
+	"""
+
+	result = optimize.minimize(fwi_loss, m0, 
+				args=(geometry0, obs, misfit_func, filt_func, bathy_mask, precond), 
+				method='L-BFGS-B', jac=True, 
+	    		callback=fwi_callback, bounds=bounds, 
+	    		options={'ftol':ftol, 'maxiter':maxiter, 'disp':True,
+	    				'eps':stepsize,
+	    				'maxls':maxls, 'gtol':gtol, 'iprint':1,
+	    		})
+
+	# Plot FWI result
+	vp = 1.0/np.sqrt(result['x'].reshape(shape))
+
+	vp.tofile(os.path.join(result_dir, "marmousi_result_misfit_"+str(misfit_type)))
+	with open(os.path.join(result_dir, "marmousi_model_err_info_"+str(misfit_type)+'.txt')) as file:
+		for item in model_err:
+			f.write("%s\n" % str(item))
+	try:
+		useful_info = []
+		with open('nohup.out', 'rt') as file:
+			for line in file:
+				if line.find('Operator') < 0:
+					useful_info.append(line)
+		with open(os.path.join(result_dir, 'marmousi_optim_info_'+str(misfit_type)+'.txt')) as file:
+			for item in useful_info:
+				f.write("%s\n" % item)
+		nohup_file = 'marmousi_nohup_'+str(misfit_type)+'.out'
+		os.rename('nohup.out', nohup_file)
+		shutil.move(nohup_file, os.path.join(result_dir, nohup_file))
+	except:
+		pass
+	plot_image(vp, vmin=vmin, vmax=vmax, cmap="jet")
